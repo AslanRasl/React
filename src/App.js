d968272fc12efc9435655c3c12610d6cdbf94330
AslanRasl <davdvrasulov75753455@gmail.com>
2024-03-05 23:16:52 +0500

    React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import LogoImg from "./logo.svg"
+import WatchImg from "./watch.svg"
+import StrImg from "./str.png"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div className="header">
+      <div className="logo">
+        <img src={LogoImg} />
+      </div>
+      <div className="nav">
+        <p className="nav__item">Home</p>
+        <p className="nav__item">about</p>
+        <p className="nav__item">store</p>
+        <p className="nav__item">contact</p>
+      </div>
     </div>
+    <div className="display">
+    <div className="hero__section">
+        <div className="text__content">
+          <h1 className="title">Tech For Real Life</h1>
+          <h5 className="subtitle">Get it all done right from your wrist.</h5>
+          <h3 className="add__text">Gen 5 Smartwatch The Carlyle HR White Silicone</h3>
+          <h3 className="price">₹ 22,995.00</h3>
+          <h5 className="text">(Price Inclusive of GST)</h5>
+          <button className="btn">buy now</button>
+        </div>
+      </div>
+      <div className="img__content">
+        <img src={WatchImg} />
+        <button className="btn__2"><img className="" src={StrImg} /></button>
+        </div>
+        </div></>
   );
 }
 
